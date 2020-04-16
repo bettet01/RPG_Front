be01ffab391b01d5b2d9025d3a124361c026e6f5
bettet01 <bettet01@luther.edu>
2020-04-16 11:48:09 -0500

    sending to jackie

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+// css
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col"></div>
+        <div className="col">Hey Jackie</div>
+        <div className="col"></div>
+      </div>
+      <div className="row">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
