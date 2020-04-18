94d75e2552e1cb832b73c2b35e4427d5c4d87243
Jackie <jackie.cychosz@knode.app>
2020-04-18 12:00:22 -0500

    changed background and created bs description in about

--- src/App.js
+++ src/App.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
-import About from './pages/About';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import React from "react";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import About from "./pages/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // css
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import "./App.css";
 
 function App() {
   return (
-<Router>
-<Container>
-    <Header/>
-    <Switch>
-    <Route path="/signup">
-            <SignUp/>
+    <Router>
+      <Container>
+        <Header />
+        <Switch>
+          <Route path="/signup">
+            <SignUp />
           </Route>
-    <Route path="/about">
-      <About/>
-    </Route>
-    </Switch>
-</Container>   
-
-</Router> 
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,26 +1,28 @@
-import React from 'react';
-
+import React from "react";
 
 // css
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card'
-import '../App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import "../App.css";
 
 function About() {
-    return (
-        <Row>
-            <Col xs={2}></Col>
-            <Col xs={8}>
-                <Row className="justify-content-md-center">
-                <h id="description-header">Game Description</h>
-                </Row>
-                <Card>
-                    <Card.Text id="card">The Dev10 game is an interactive experience...</Card.Text>
-                </Card>
-            </Col>
-            <Col xs={2}></Col>
+  return (
+    <Row>
+      <Col xs={2}></Col>
+      <Col xs={8}>
+        <Row className="justify-content-md-center">
+          <h id="description-header">Game Description</h>
         </Row>
-    ); }
-    export default About;
+        <Card id="card">
+          <Card.Text id="card-text">
+            The Dev10 game is a fun interactive experience that I developed and refined over the past two weeks. The user starts off with only a sword to protect themselves, but as they defeat more foes they acquire new equipment and gradually level up. The equipment includes weapons such as maces and swords and potions that allow you to restore your health. Your foes are skeletons and they usually attack you one at a time. However, if you are so unlucky, they will come at you in packs. The objective of the game is to become as OP as possible.
+          </Card.Text>
+        </Card>
+      </Col>
+      <Col xs={2}></Col>
+    </Row>
+  );
+}
+export default About;
