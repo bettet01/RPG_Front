2ff0da2b828fb91905133bd673cfbf3f233e4d93
bettet01 <bettet01@luther.edu>
2020-04-18 11:53:46 -0500

    some interaction done on signup

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -8,18 +8,66 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import '../App.css';
+import axios from 'axios';
 
 function SignUp() {
+  const [data, setData] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: ""
+  });
+
+  const changeUsername = (e) => {
+    setData({...data, username: e.target.value});
+
+    if(data.username.length <= 3){
+      var x = document.getElementById("span-4-characters");
+      x.style.color = "red";
+    } else {
+      var y = document.getElementById("span-4-characters");
+      y.style.color = "green";
+    }
+  }
+
+  const changeEmail = (e) => {
+    setData({...data, email: e.target.value});
+  }
+
+  const changePassword = (e) => {
+    setData({...data, password: e.target.value});
+  }
+
+  const changeConfirmPassword = (e) => {
+    setData({...data, comfirmPassword: e.target.value});
+  }
+
+  const submitData = (e) => {
+    axios.post({
+      method: 'post',
+      url: 'localhost:8080/users/user',
+      data: {
+        username: data.username,
+        password: data.password,
+        email: data.email
+      }  
+    }).then(() => {
+      // do stuff
+    }).catch(() => {
+      // do something
+    })
+  } 
+
     return (
         <Row className="Row-2">
         <Col></Col>
         <Col xs={6}>
           <h1 id="sign-up-col">Sign Up</h1>
-        <Form>
+        <Form onSubmit={submitData}>
         <Form.Group id="form-group-username">
             <Form.Label id="label">
             Username</Form.Label>
-            <Form.Control id="form-control-username" type="username" placeholder="Username" />
+            <Form.Control onChange={changeUsername} value={data.username} id="form-control-username" type="username" placeholder="Username" />
             <Form.Text id="form-text-username"> 
               <span id="span-4-characters"> 4 Characters Minimum</span> |
               <span id="span-no-special"> No Special Characters</span>
@@ -28,13 +76,13 @@ function SignUp() {
     
           <Form.Group id="form-group-email">
             <Form.Label id="label">Email address</Form.Label>
-            <Form.Control id="form-control-email" placeholder="Email" />
+            <Form.Control onChange={changeEmail} value={data.email} id="form-control-email" placeholder="Email" />
           </Form.Group>
     
           <Form.Group id="form-group-password">
             <Form.Label id="label">
             Password</Form.Label>
-            <Form.Control id="form-control-password" type="password" placeholder="Password" />
+            <Form.Control onChange={changePassword} value={data.password} id="form-control-password" type="password" placeholder="Password" />
             <Form.Text id="form-text-password">
               <span id="span-uppercase"> 1 Uppercase Letter</span> | 
               <span id="span-lowercase"> 1 Lowercase Letter</span> |
@@ -46,7 +94,7 @@ function SignUp() {
           <Form.Group id="form-group-password">
             <Form.Label id="label">
             Confirm Password</Form.Label>
-            <Form.Control id="form-control-password" type="password" placeholder="Confirm Password" />
+            <Form.Control onChange={changeConfirmPassword} value={data.confirmPassword} id="form-control-password" type="password" placeholder="Confirm Password" />
             <Form.Text>Passwords Match</Form.Text>
           </Form.Group>
     
