1f80765ef1fb808e7a3dc50f1ea856e613d1f953
Jackie <jackie.cychosz@knode.app>
2020-04-22 15:39:18 -0500

    Created Delete User Page with Table & Red Delete Account Button

--- src/components/Deleteuser.js
+++ src/components/Deleteuser.js
@@ -4,14 +4,43 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../App.css";
 import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 
 function Deleteuser() {
   return (
-      <Row>
-          <Col>hello</Col>
-          <p>hello</p>
+    <div>
+      <Table id="user-profile" variant="dark" bordered>
+        <tbody>
+          <tr>
+            <td>Account Username</td>
+            <td>Account Username Here</td>
+          </tr>
+          <tr>
+            <td>Account Email</td>
+            <td>Account Email Here</td>
+          </tr>
+          <tr>
+            <td>Account Level</td>
+            <td>Account Level Here</td>
+          </tr>
+          <tr>
+            <td>Account Weapons</td>
+            <td>Account Weapons Here</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Row className="justify-content-md-center">
+        <Button
+          className="mx-auto"
+          id="submit-button"
+          variant="danger"
+          type="submit"
+        >
+          Delete Account
+        </Button>
       </Row>
+    </div>
   );
 }
 
